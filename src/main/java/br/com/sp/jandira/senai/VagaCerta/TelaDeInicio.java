25b949330498ea802f42d91da3ea717c76455966
HUGO DE OLIVEIRA ALVES VIEIRA <25203638@CFP127ALUNO.LOCAL>
2025-12-09 10:34:10 -0300

    inicio do javafx

--- src/main/java/br/com/sp/jandira/senai/VagaCerta/TelaDeInicio.java
+++ src/main/java/br/com/sp/jandira/senai/VagaCerta/TelaDeInicio.java
@@ -1,4 +1,24 @@
 package br.com.sp.jandira.senai.VagaCerta;
 
-public class TelaDeInicio {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class TelaDeInicio extends Application {
+
+    Label label = new Label();
+
+    @Override
+    public void start(Stage stage) {
+
+        stage.setTitle(" Vaga Certa");
+        Scene scene = new Scene(label, 400, 400);
+
+        Label titulo = new Label("Vaga Certa");
+
+        stage.getScene();
+
+        stage.show();
+    }
 }
