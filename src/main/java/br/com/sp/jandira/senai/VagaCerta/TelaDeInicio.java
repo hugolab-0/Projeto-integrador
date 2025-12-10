9cf817c7ee5662dc322bfa7435cfbad9c44a6f23
Hug-lab0 <hugoalvesvieira0@gmail.com>
2025-12-09 21:20:06 -0300

    arrumando as novidades na interface

--- src/main/java/br/com/sp/jandira/senai/VagaCerta/TelaDeInicio.java
+++ src/main/java/br/com/sp/jandira/senai/VagaCerta/TelaDeInicio.java
@@ -1,8 +1,10 @@
 package br.com.sp.jandira.senai.VagaCerta;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.application.Application;
@@ -24,18 +26,40 @@ public class TelaDeInicio extends Application {
 
 
         // ------- ELEMENTOS DA PAGINA -------
-        Button btnAddUser = new Button ("\uD83D\uDCDD");
-        Button btnExcluir = new Button("\uD83D\uDCDD");
-        Button btnRegistro = new Button("\uD83D\uDCC3");
-        Button btnSair = new Button("\uD83D\uDCDD");
-        Label titulo = new Label("Bem-Vindo");
+        Button btnAddUser = new Button ("\uD83D\uDC64");
+        Button btnExcluir = new Button("\uD83D\uDDD1");
+        Button btnRegistro = new Button("\uD83D\uDCDC");
+        Button btnSair = new Button("\uD83C\uDFC3\u200D");
+        Label titulo = new Label("BEM-VINDO");
+
+        // ------------- AJUSTE NOS ELEMENTOS --------
+
+        titulo.setStyle("-fx-font-size: 40px");
+
+        btnAddUser.setStyle("-fx-text-alignment: center; -fx-font-size: 35px;");
+        btnExcluir.setStyle("-fx-text-alignment: center; -fx-font-size: 35px;");
+        btnRegistro.setStyle("-fx-text-alignment: center; -fx-font-size: 35px;");
+        btnSair.setStyle("-fx-text-alignment: center; -fx-font-size: 35px;");
+
+        // ------- CAIXA PRINCIPAL HORIZONTAL (HBox) -------
+        HBox caixaHorizontal = new HBox();
+        caixaHorizontal.setPadding(new Insets(12));
+        caixaHorizontal.getChildren().addAll(btnAddUser, btnExcluir, btnRegistro, btnSair);
 
         // ------- CAIXA PRINCIPAL VERTICAL (VBox) -------
-        VBox caixaPrincipal = new VBox(15); // espaçamento 15px
-        caixaPrincipal.getChildren().addAll(btnAddUser, btnExcluir, btnRegistro, btnSair, titulo);
+        VBox caixaPrincipal = new VBox(15);
+        caixaPrincipal.setStyle("-fx-gap-start-and-end: 10px, 20px, 10px");
+        caixaPrincipal.getChildren().addAll(caixaHorizontal);
+
+        // -------- CRIANDO CONTAINER -----------
+        StackPane root = new StackPane();
+        root.setStyle("-fx-background-color: pink");
+        root.setPadding(new Insets(20));
+        root.getChildren().addAll(caixaPrincipal, titulo);
+
 
         // ------- CRIAR A CENA -------
-        Scene cena = new Scene(caixaPrincipal, 650, 350);
+        Scene cena = new Scene(root, 650, 550);
 
         // ------- MOSTRAR A JANELA -------
         stage.setScene(cena);
