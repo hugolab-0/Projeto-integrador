fd60632259fc1429ecae3f172a8c5c6085fd5814
Hug-lab0 <hugoalvesvieira0@gmail.com>
2025-12-09 22:04:06 -0300

    inicio da tela de cadastro

--- src/main/java/br/com/sp/jandira/senai/VagaCerta/TelaDeCadastro.java
+++ src/main/java/br/com/sp/jandira/senai/VagaCerta/TelaDeCadastro.java
@@ -1,7 +1,89 @@
 package br.com.sp.jandira.senai.VagaCerta;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class TelaDeCadastro {
-    TextField campoTexto = new TextField();
+public class TelaDeCadastro extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        stage.setTitle(" Vaga Certa");
+
+        // ------- ELEMENTOS DA PAGINA -------
+        Button btnAddUser = new Button ("\uD83D\uDC64");
+        Button btnExcluir = new Button("\uD83D\uDDD1");
+        Button btnRegistro = new Button("\uD83D\uDCDC");
+        Button btnSair = new Button("\uD83C\uDFC3\u200D");
+        Label titulo = new Label("BEM-VINDO");
+
+        // ------------- AJUSTE NOS ELEMENTOS --------
+
+        titulo.setStyle("-fx-font-size: 50px; -fx-text-fill: #F4F0F0");
+
+        String buttonStyle = "-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0";
+        btnAddUser.setStyle(buttonStyle);
+        btnExcluir.setStyle(buttonStyle);
+        btnRegistro.setStyle(buttonStyle);
+        btnSair.setStyle(buttonStyle);
+
+        btnAddUser.setOnAction(e -> {
+            btnAddUser.setStyle( "-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #FFB800; -fx-border-color: #FFB800");
+            btnExcluir.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+            btnRegistro.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+            btnSair.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+        });
+        btnExcluir.setOnAction(e -> {
+            btnAddUser.setStyle( "-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+            btnExcluir.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #FFB800; -fx-border-color: #FFB800");
+            btnRegistro.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+            btnSair.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+        });
+        btnRegistro.setOnAction(e -> {
+            btnAddUser.setStyle( "-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+            btnExcluir.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+            btnRegistro.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #FFB800; -fx-border-color: #FFB800");
+            btnSair.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+        });
+        btnSair.setOnAction(e -> {
+            btnAddUser.setStyle( "-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+            btnExcluir.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+            btnRegistro.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #F4F0F0;");
+            btnSair.setStyle("-fx-font-family: 'Segoe UI Emoji'; -fx-font-size: 35px; -fx-background-color: #322f32; -fx-text-fill: #FFB800; -fx-border-color: #FFB800");
+        });
+
+
+        // ------- CAIXA PRINCIPAL HORIZONTAL (HBox) -------
+        HBox caixaHorizontal = new HBox(15);
+        caixaHorizontal.setPadding(new Insets(12));
+        caixaHorizontal.getChildren().addAll(btnAddUser, btnExcluir, btnRegistro, btnSair);
+
+        // ------- CAIXA PRINCIPAL VERTICAL (VBox) -------
+        VBox caixaPrincipal = new VBox(15);
+        caixaPrincipal.setStyle("-fx-background-color: #49414B");
+        caixaPrincipal.getChildren().addAll(caixaHorizontal);
+
+        // -------- CRIANDO CONTAINER -----------
+        StackPane root = new StackPane();
+        root.setStyle("-fx-background-color: #323031");
+        root.setPadding(new Insets(20));
+        root.getChildren().addAll(caixaPrincipal, titulo);
+
+
+        // ------- CRIAR A CENA -------
+        Scene cena = new Scene(root, 750, 550);
+
+        // ------- MOSTRAR A JANELA -------
+        stage.setScene(cena);
+        stage.show();
+    }
 }
+
