71f78f8e8773c5558e4360cd6cb567fdbcaf5105
Hug-lab0 <hugoalvesvieira0@gmail.com>
2025-12-10 12:04:31 -0300

    criação de novos elementos

--- src/main/java/br/com/sp/jandira/senai/VagaCerta/TelaDeCadastro.java
+++ src/main/java/br/com/sp/jandira/senai/VagaCerta/TelaDeCadastro.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -25,15 +26,59 @@ public class TelaDeCadastro extends Application {
         Button btnSair = new Button("\uD83C\uDFC3\u200D");
 
         // ------- ELEMENTOS DA PAGINA -------
+        Label nome = new Label("Nome do proprietário");
+        nome.setStyle("-fx-font-size: 16px; -fx-font-family: Arial");
 
+       TextField campoNome = new TextField();
+        campoNome.setPromptText("Nome");
+        campoNome.setMaxWidth(200);
+        campoNome.setPrefHeight(30);
+
+        Label modelo = new Label("Modelo do veículo");
+        modelo.setStyle("-fx-font-size: 16px; -fx-font-family: Arial");
+
+        TextField campoModelo = new TextField();
+        campoModelo.setPromptText("Modelo");
+        campoModelo.setMaxWidth(200);
+        campoModelo.setPrefHeight(30);
+
+        Label placa = new Label("Placa do veículo");
+        placa.setStyle("-fx-font-size: 16px; -fx-font-family: Arial");
+
+        TextField campoPlaca = new TextField();
+        campoPlaca.setPromptText("Placa");
+        campoPlaca.setMaxWidth(200);
+        campoPlaca.setPrefHeight(30);
+
+        Label cor = new Label("Cor do veículo");
+        cor.setStyle("-fx-font-size: 16px; -fx-font-family: Arial");
+
+        TextField campoCor = new TextField();
+        campoCor.setPromptText("Cor");
+        campoCor.setMaxWidth(200);
+        campoCor.setPrefHeight(30);
 
         //--------- CRIAÇÃO DE COLETA DE INFORMAÇÕES ----------
 
-        HBox caixaDeInformacoes = new HBox();
+        VBox caixaDeInformacoes = new VBox(10);
         caixaDeInformacoes.setStyle("-fx-background-color: blue");
-        caixaDeInformacoes.setPadding(new Insets(10, 10, 10, 10));
-        caixaDeInformacoes.setMaxWidth(400);
+        VBox.setMargin(nome,new Insets(8, 0, 0, 10));
+        VBox.setMargin( campoNome,new Insets(0, 0, 0, 10));
+        caixaDeInformacoes.setPrefHeight(92);
+        caixaDeInformacoes.setMaxWidth(300);
+        caixaDeInformacoes.getChildren().addAll(nome, campoNome);
 
+        VBox caixaDeInformacoesCar = new VBox(12);
+        caixaDeInformacoesCar.setStyle("-fx-background-color: #d10d0d");
+        VBox.setMargin( modelo, new Insets(8, 0, 0, 10));
+        VBox.setMargin( placa, new Insets(8, 0, 0, 10));
+        VBox.setMargin( cor, new Insets(8, 0, 0, 10));
+        VBox.setMargin( campoModelo,new Insets(0, 0, 0, 10));
+        VBox.setMargin( campoPlaca,new Insets(0, 0, 0, 10));
+        VBox.setMargin( campoCor,new Insets(0, 0, 0, 10));
+        caixaDeInformacoesCar.setPrefHeight(325);
+        caixaDeInformacoesCar.setMaxWidth(350);
+        caixaDeInformacoesCar.getChildren().addAll(modelo, campoModelo, placa, campoPlaca, cor, campoCor);
 
 
         // ------------- AJUSTE NOS ELEMENTOS --------
@@ -79,9 +124,12 @@ public class TelaDeCadastro extends Application {
         VBox caixaPrincipal = new VBox(15);
         caixaPrincipal.setStyle("-fx-background-color: #49414B");
         VBox.setMargin(caixaDeInformacoes, new Insets(20, 0, 0, 35));
-        caixaPrincipal.getChildren().addAll(caixaHorizontal, caixaDeInformacoes);
+        VBox.setMargin(caixaDeInformacoesCar, new Insets(10, 0, 0, 35));
+
+        caixaPrincipal.getChildren().addAll(caixaHorizontal, caixaDeInformacoes, caixaDeInformacoesCar);
 
         // -------- CRIANDO CONTAINER -----------
+
         StackPane root = new StackPane();
         root.setStyle("-fx-background-color: #323031");
         root.setPadding(new Insets(20));
@@ -89,7 +137,7 @@ public class TelaDeCadastro extends Application {
 
 
         // ------- CRIAR A CENA -------
-        Scene cena = new Scene(root, 750, 550);
+        Scene cena = new Scene(root, 850, 650);
 
         // ------- MOSTRAR A JANELA -------
         stage.setScene(cena);
