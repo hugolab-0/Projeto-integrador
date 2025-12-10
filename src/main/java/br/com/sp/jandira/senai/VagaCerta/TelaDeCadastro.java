c766ce1b727c1559442522e473100dd4ceb102ec
Hug-lab0 <hugoalvesvieira0@gmail.com>
2025-12-10 12:43:49 -0300

    instalando o botao de registrar

--- src/main/java/br/com/sp/jandira/senai/VagaCerta/TelaDeCadastro.java
+++ src/main/java/br/com/sp/jandira/senai/VagaCerta/TelaDeCadastro.java
@@ -27,41 +27,49 @@ public class TelaDeCadastro extends Application {
 
         // ------- ELEMENTOS DA PAGINA -------
         Label nome = new Label("Nome do proprietário");
-        nome.setStyle("-fx-font-size: 16px; -fx-font-family: Arial");
+        nome.setStyle("-fx-font-size: 16px; -fx-font-family: 'Roboto'; -fx-text-fill: #ecdfd2");
 
        TextField campoNome = new TextField();
+        campoNome.setStyle("-fx-background-color: #ecdfd2");
         campoNome.setPromptText("Nome");
         campoNome.setMaxWidth(200);
         campoNome.setPrefHeight(30);
 
         Label modelo = new Label("Modelo do veículo");
-        modelo.setStyle("-fx-font-size: 16px; -fx-font-family: Arial");
+        modelo.setStyle("-fx-font-size: 20px; -fx-font-family: 'Roboto'; -fx-text-fill: #ecdfd2");
 
         TextField campoModelo = new TextField();
+        campoModelo.setStyle("-fx-background-color: #ecdfd2");
         campoModelo.setPromptText("Modelo");
         campoModelo.setMaxWidth(200);
         campoModelo.setPrefHeight(30);
 
         Label placa = new Label("Placa do veículo");
-        placa.setStyle("-fx-font-size: 16px; -fx-font-family: Arial");
+        placa.setStyle("-fx-font-size: 20px; -fx-font-family: 'Roboto'; -fx-text-fill: #ecdfd2");
 
         TextField campoPlaca = new TextField();
+        campoPlaca.setStyle("-fx-background-color: #ecdfd2");
         campoPlaca.setPromptText("Placa");
         campoPlaca.setMaxWidth(200);
         campoPlaca.setPrefHeight(30);
 
         Label cor = new Label("Cor do veículo");
-        cor.setStyle("-fx-font-size: 16px; -fx-font-family: Arial");
+        cor.setStyle("-fx-font-size: 20px; -fx-font-family: 'Roboto'; -fx-text-fill: #ecdfd2");
 
         TextField campoCor = new TextField();
+        campoCor.setStyle("-fx-background-color: #ecdfd2");
         campoCor.setPromptText("Cor");
         campoCor.setMaxWidth(200);
         campoCor.setPrefHeight(30);
 
+        Button btnRegistrar = new Button("Registrar");
+        btnRegistrar.setStyle("-fx-background-color: red");
+        btnRegistrar.setPadding(new Insets(15, 40, 15, 40));
+
         //--------- CRIAÇÃO DE COLETA DE INFORMAÇÕES ----------
 
         VBox caixaDeInformacoes = new VBox(10);
-        caixaDeInformacoes.setStyle("-fx-background-color: blue");
+        caixaDeInformacoes.setStyle("-fx-background-color: #3f3242");
         VBox.setMargin(nome,new Insets(8, 0, 0, 10));
         VBox.setMargin( campoNome,new Insets(0, 0, 0, 10));
         caixaDeInformacoes.setPrefHeight(92);
@@ -69,7 +77,7 @@ public class TelaDeCadastro extends Application {
         caixaDeInformacoes.getChildren().addAll(nome, campoNome);
 
         VBox caixaDeInformacoesCar = new VBox(12);
-        caixaDeInformacoesCar.setStyle("-fx-background-color: #d10d0d");
+        caixaDeInformacoesCar.setStyle("-fx-background-color: #322431");
         VBox.setMargin( modelo, new Insets(8, 0, 0, 10));
         VBox.setMargin( placa, new Insets(8, 0, 0, 10));
         VBox.setMargin( cor, new Insets(8, 0, 0, 10));
@@ -117,8 +125,9 @@ public class TelaDeCadastro extends Application {
 
         // ------- CAIXA PRINCIPAL HORIZONTAL (HBox) -------
         HBox caixaHorizontal = new HBox(15);
+        HBox.setMargin(btnRegistrar, new Insets(20, 20, 20, 20));
         caixaHorizontal.setPadding(new Insets(12));
-        caixaHorizontal.getChildren().addAll(btnAddUser, btnExcluir, btnRegistro, btnSair);
+        caixaHorizontal.getChildren().addAll(btnAddUser, btnExcluir, btnRegistro, btnSair, btnRegistrar);
 
         // ------- CAIXA PRINCIPAL VERTICAL (VBox) -------
         VBox caixaPrincipal = new VBox(15);
